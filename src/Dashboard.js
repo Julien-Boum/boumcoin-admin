b60d735641668363f2f007201ddb86236534963c
Julien-Boum <julienjarry@orange.fr>
2025-06-11 17:10:39 +0200

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,20 @@
-
 import React from 'react';
+import ImageManager from './ImageManager';
 import SocialLinksManager from './SocialLinksManager';
+import BuyButtonManager from './BuyButtonManager';
+import MultilangTextManager from './MultilangTextManager';
 
 function Dashboard() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Panneau d'administration Boumcoin</h1>
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1 style={{ textAlign: 'center' }}>🛠️ Panneau d'administration Boumcoin</h1>
+
+      <ImageManager />
       <SocialLinksManager />
+      <BuyButtonManager />
+      <MultilangTextManager />
     </div>
   );
 }
+
 export default Dashboard;
